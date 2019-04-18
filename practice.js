07dd36946ac3aa952155bc03ff4758f9290e2e24
Jeff Nguyen <jeffnguyener@Samanthas-MacBook-Pro.local>
2019-04-18 17:53:12 -0600

    Updated prob 6

--- practice.js
+++ practice.js
@@ -129,7 +129,20 @@ var numbersArray = [1, 2, 34, 54, 55, 34, 32, 11, 19, 17, 54, 66, 13];
 
 //Code Here
 
-
+function divider(numbersArray) {
+  var evensArray = [];
+  var oddsArray = [];
+  var output = [];
+    for (var i = 0; i < numbersArray.length; i++) {
+      if (numbersArray[i] % 2 === 0) {
+        evensArray.push(numbersArray[i]);
+      } else {
+        oddsArray.push(numbersArray[i]);
+      }
+    }
+    output.push(evensArray, oddsArray);
+    return output;
+}
 
 ////////// PROBLEM 7 //////////
 
