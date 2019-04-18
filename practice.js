b5ed4d9efa18b5c2aeaf8e07fc281d10a29d732f
Jeff Nguyen <jeffnguyener@Samanthas-MacBook-Pro.local>
2019-04-17 22:17:28 -0600

    Prob 5 solved

--- practice.js
+++ practice.js
@@ -85,11 +85,14 @@ var nums = [1, 2, 3, 6, 22, 98, 45, 23, 22, 12];
   Return an array that contains the even numbers from the nums array.
 */
 
-//Code Here
-
-
-
-
+function evenFinder(nums) {
+  var evenNums = [];
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] % 2 === 0) {
+      evenNums.push(nums[i]);
+    }
+  } return evenNums
+}
 
 
 
